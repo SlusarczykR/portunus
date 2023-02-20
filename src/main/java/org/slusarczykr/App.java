8ad96fd2054ed4a5b03aa69af9dbf1c714d584ae
SlusarczykR <radoslaw.slusarczyk95@gmail.com>
2023-02-20 17:43:05 +0100

    initial commit

---
+++ src/main/java/org/slusarczykr/App.java
@@ -0,0 +1,13 @@
+package org.slusarczykr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
