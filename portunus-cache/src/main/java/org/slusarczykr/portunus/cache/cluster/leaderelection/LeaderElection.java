d91a33a8007a83aed63db3218e7697b72c44ae57
SlusarczykRad <radoslaw.slusarczyk@leovegas.com>
2023-03-12 13:55:15 +0100

    add initial LeaderElection component

---
+++ portunus-cache/src/main/java/org/slusarczykr/portunus/cache/cluster/leaderelection/LeaderElection.java
@@ -0,0 +1,12 @@
+package org.slusarczykr.portunus.cache.cluster.leaderelection;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class LeaderElection implements Watcher {
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
+}
