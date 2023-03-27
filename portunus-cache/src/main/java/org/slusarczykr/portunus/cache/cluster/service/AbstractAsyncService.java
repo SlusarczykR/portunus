05a37699f837cb0933fbeb8f6115f68a7f51c88c
SlusarczykRad <radoslaw.slusarczyk@leovegas.com>
2023-03-27 09:37:32 +0200

    remove redundant logger

--- portunus-cache/src/main/java/org/slusarczykr/portunus/cache/cluster/service/AbstractAsyncService.java
+++ portunus-cache/src/main/java/org/slusarczykr/portunus/cache/cluster/service/AbstractAsyncService.java
@@ -1,15 +1,10 @@
 package org.slusarczykr.portunus.cache.cluster.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slusarczykr.portunus.cache.cluster.event.publisher.DefaultClusterEventPublisher;
 import org.slusarczykr.portunus.cache.maintenance.AbstractManaged;
 
 import java.util.concurrent.ExecutorService;
 
 public abstract class AbstractAsyncService extends AbstractManaged implements AsyncService {
-
-    private static final Logger log = LoggerFactory.getLogger(AbstractAsyncService.class);
 
     private final ExecutorService innerExecutor;
 
