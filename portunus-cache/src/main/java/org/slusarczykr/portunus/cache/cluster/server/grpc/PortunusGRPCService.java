ee61174af5ba6464140fa9adf3a5053f5a52d16d
SlusarczykRad <radoslaw.slusarczyk@leovegas.com>
2023-04-01 19:38:15 +0200

    add 'removeEntry' method impl. to PortunusGRPCService

--- portunus-cache/src/main/java/org/slusarczykr/portunus/cache/cluster/server/grpc/PortunusGRPCService.java
+++ portunus-cache/src/main/java/org/slusarczykr/portunus/cache/cluster/server/grpc/PortunusGRPCService.java
@@ -1,5 +1,6 @@
 package org.slusarczykr.portunus.cache.cluster.server.grpc;
 
+import com.google.protobuf.ByteString;
 import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
 import lombok.SneakyThrows;
@@ -12,6 +13,8 @@ import org.slusarczykr.portunus.cache.api.command.PortunusCommandApiProtos.GetPa
 import org.slusarczykr.portunus.cache.api.command.PortunusCommandApiProtos.GetPartitionsDocument;
 import org.slusarczykr.portunus.cache.api.command.PortunusCommandApiProtos.PutEntryCommand;
 import org.slusarczykr.portunus.cache.api.command.PortunusCommandApiProtos.PutEntryDocument;
+import org.slusarczykr.portunus.cache.api.command.PortunusCommandApiProtos.RemoveEntryCommand;
+import org.slusarczykr.portunus.cache.api.command.PortunusCommandApiProtos.RemoveEntryDocument;
 import org.slusarczykr.portunus.cache.api.event.PortunusEventApiProtos.ClusterEvent;
 import org.slusarczykr.portunus.cache.api.query.PortunusQueryApiProtos.ContainsAnyEntryDocument;
 import org.slusarczykr.portunus.cache.api.query.PortunusQueryApiProtos.ContainsAnyEntryQuery;
@@ -65,7 +68,7 @@ public class PortunusGRPCService extends PortunusServiceImplBase {
 
     @SneakyThrows
     private ContainsEntryDocument createContainsEntryDocument(ContainsEntryQuery query) {
-        Distributed<?> distributed = wrap(query);
+        Distributed<?> distributed = getDistributedKey(query);
         boolean containsEntry = containsEntry(query, distributed);
         return ContainsEntryDocument.newBuilder()
                 .setContainsEntry(containsEntry)
@@ -83,12 +86,16 @@ public class PortunusGRPCService extends PortunusServiceImplBase {
         return cache.containsKey(key);
     }
 
-    private <T extends Serializable> Distributed<T> wrap(ContainsEntryQuery query) throws PortunusException {
+    private <T extends Serializable> Distributed<T> getDistributedKey(ContainsEntryQuery query) throws PortunusException {
         try {
-            return Distributed.DistributedWrapper.fromBytes(query.getEntryKey().toByteArray());
+            return toDistributed(query.getEntryKey());
         } catch (Exception e) {
             throw new PortunusException("Unable to deserialize request");
         }
+    }
+
+    private <K extends Serializable> Distributed<K> toDistributed(ByteString bytes) {
+        return Distributed.DistributedWrapper.fromBytes(bytes.toByteArray());
     }
 
     @Override
@@ -142,10 +149,37 @@ public class PortunusGRPCService extends PortunusServiceImplBase {
         Cache<K, V> cache = cacheManager.getCache(command.getCacheName());
         Cache.Entry<K, V> entry = clusterService.getConversionService().convert(command.getCacheEntry());
         cache.put(entry);
-        
+
         return PutEntryDocument.newBuilder()
                 .setStatus(true)
                 .build();
+    }
+
+    @Override
+    public void removeEntry(RemoveEntryCommand request, StreamObserver<RemoveEntryDocument> responseObserver) {
+        responseObserver.onNext(removeEntry(request));
+        responseObserver.onCompleted();
+    }
+
+    @SneakyThrows
+    private <K extends Serializable, V extends Serializable> RemoveEntryDocument removeEntry(RemoveEntryCommand command) {
+        Cache<K, V> cache = cacheManager.getCache(command.getCacheName());
+        Distributed<K> distributed = toDistributed(command.getKey());
+
+        return cache.getEntry(distributed.get())
+                .map(it -> {
+                    removeEntry(cache, it.getKey());
+                    return RemoveEntryDocument.newBuilder()
+                            .setCacheEntry(clusterService.getConversionService().convert(it))
+                            .build();
+
+                })
+                .orElseThrow(() -> new PortunusException("Could not remove entry"));
+    }
+
+    @SneakyThrows
+    private <K extends Serializable, V extends Serializable> Cache.Entry<K, V> removeEntry(Cache<K, V> cache, K key) {
+        return cache.remove(key);
     }
 
     @Override
